39f511a7d3ddc5fd9d307e8dda4b6d03cf76b2f5
LT-LuciaT <lucia.trombin98@gmail.com>
2025-01-24 12:33:16 +0100

    3

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 const STRIVEschool = "https://striveschool-api.herokuapp.com/api/product/";
 const APIkey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNWIzMmI3NDcwMTAwMTU4YjJhZmEiLCJpYXQiOjE3Mzc3MTAzODYsImV4cCI6MTczODkxOTk4Nn0.RPtjlDt--oxvJSpmA0Fo6zhhWpskj5mdw7LWtohunC4";
+
+document.getElementById("productForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const newProduct = {
+    name: document.getElementById("name").value,
+    description: document.getElementById("description").value,
+    brand: document.getElementById("brand").value,
+    price: parseFloat(document.getElementById("price").value),
+    imageUrl:
+      document.getElementById("imageUrl").value.trim() === "" ? null : document.getElementById("imageUrl").value, // Se l'URL immagine non è stato inserito, impostiamo null
+  };
+  fetch(STRIVEschool, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${APIkey}`,
+    },
+    body: JSON.stringify(newProduct),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((errorData) => {
+          throw new Error(`Errore: ${JSON.stringify(errorData)}`);
+        });
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Prodotto aggiunto con successo:", data);
+      alert("Prodotto aggiunto con successo!");
+      document.getElementById("productForm").reset();
+    })
+    .catch((error) => {
+      console.error("Errore nell'aggiungere il prodotto:", error);
+      alert("Errore nell'aggiungere il prodotto: " + error.message);
+    });
+});
