a4c39731971306df21a9d9a58772244592560b6e
LT-LuciaT <lucia.trombin98@gmail.com>
2025-01-24 11:14:33 +0100

    1

---
+++ script.js
@@ -0,0 +1,3 @@
+const STRIVEschool = "https://striveschool-api.herokuapp.com/api/product/";
+const APIkey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNWIzMmI3NDcwMTAwMTU4YjJhZmEiLCJpYXQiOjE3Mzc3MTAzODYsImV4cCI6MTczODkxOTk4Nn0.RPtjlDt--oxvJSpmA0Fo6zhhWpskj5mdw7LWtohunC4";
